880739eef1e09415b135bdaedf5db1ee7eb16e36
mrshuzaifa <qanitahuzaifa@gmail.com>
2024-07-03 14:00:31 +0500

    invoices dashboard feature completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,16 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
  
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'Acme Invoice Dashboard, built with App Router.',
+  metadataBase: new URL('https://nextjs-dashboard-ten-eta-13.vercel.app/'),
+};
+
 export default function RootLayout({
   children,
 }: {
